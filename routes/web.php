dde8cd3e3682c6d1b583a872383b97e946143366
raihananhar <203040113@mail.unpas.ac.id>
2022-11-05 16:03:35 +0700

    tugas video 17

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardPostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -63,21 +63,9 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', function() {
+    return view('dashboard.index');
+})->middleware('auth');
 
-// Route::get('/categories/{category:slug}', function(Category $category) {
-//     return view('posts', [
-//         'title' => "Post By Category : $category->name",
-//         "active" => "categories",
-//         'posts' => $category->posts->load('category', 'author')
 
-//     ]);
-// });
-
-// Route::get('/authors/{author:username}', function(User $author) {
-//     return view('posts', [
-//         'title' => "Post By Author : $author->name",
-//         "active" => "blog",
-//         'posts' => $author->posts->load('category', 'author')
-//     ]);
-// });
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
