ace913561290c3e7a07588e3d9f1c1755c16ced8
raihananhar <203040113@mail.unpas.ac.id>
2022-11-05 15:36:25 +0700

    tugas video 15

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Unique;
 
 class RegisterController extends Controller
 {
@@ -13,4 +16,24 @@ class RegisterController extends Controller
             'active' => 'register'
         ]);
     }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:255'
+        ]);
+
+        // $validatedData['password'] = bcrypt($validatedData['password']);
+
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+
+        // $request->session()->flash('success', 'Registration success, please login');
+        return redirect('/login')->with('success', 'Registration success, please login');
+    }
+
 }
